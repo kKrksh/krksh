576fe5cff8eeac21055fd7dec7152eda62daaeb1
kKrksh <151066724+kKrksh@users.noreply.github.com>
2024-02-29 18:53:53 +0100

    Add files via upload

---
+++ gluecksrad/scirpt.js
@@ -0,0 +1,21 @@
+const Namen = ["Ali", "Linus", "Karam", "Thomas"];
+
+function spin() {
+  let counter = 0;
+  let previousIndex = -1;
+
+  let intervalId = setInterval(() => {
+    if (counter < 40) {
+      let newIndex;
+      do {
+        newIndex = Math.floor(Math.random() * Namen.length);
+      } while (newIndex === previousIndex);
+      previousIndex = newIndex;
+      document.getElementById("name").textContent = Namen[newIndex];
+      counter++;
+    } else {
+      clearInterval(intervalId);
+      document.getElementById("name").textContent = "Ali";
+    }
+  }, 100);
+}
