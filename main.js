d05792cf5669f57584291c7a45178085fda6b2be
kKrksh <151066724+kKrksh@users.noreply.github.com>
2024-03-31 16:14:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ let current = 1;
 const displayer = document.getElementById("display");
 let color = "white"
 function scrollForward() {
+  document.getElementById(`c${current}`).style = "color:black;";
   if (current === 4) {
     current = 0;
   }
   current++;
   displayer.src = `media/image${current}${color}.jpg`;
+  document.getElementById(`c${current}`).style = "color:white;";
 }
 function scrollBackwards() {
+  document.getElementById(`c${current}`).style = "color:black;";
   if (current === 1) {
     current = 5;
   }
   current--;
   displayer.src = `media/image${current}${color}.jpg`;
+  document.getElementById(`c${current}`).style = "color:white;";
 }
 function colorSelection(col) {
   color = col
