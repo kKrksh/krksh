ffd110c08cad0721f12ba74b65ae4fadb49ee23d
kKrksh <151066724+kKrksh@users.noreply.github.com>
2024-04-07 16:18:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,35 @@ function colorSelection(col) {
   current = 1;
   displayer.src = `media/image${current}${color}.jpg`;
 }
+var hasTouchScreen = false;
+
+if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+} else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ('orientation' in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+        // Only as a last resort, fall back to user agent sniffing
+        var UA = navigator.userAgent;
+        hasTouchScreen = (
+            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+        );
+    }
+}
+
+if (hasTouchScreen){
+    document.getElementById("body").textContent = "This site is unavailable for mobile devices, please switch to a desktop device to access";
+}
+else{
+  console.log("working?")
+}
+
 const isMobile = navigator.userAgentData.mobile;
 if (isMobile) {
     document.getElementById('display').style = 'height : 100%;'
